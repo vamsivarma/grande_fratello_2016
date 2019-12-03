d953381f65eb323c10f79d5cf6b1151ac047e226
Vamsi Krishna Varma <vamsi.gunturi@gmail.com>
2019-12-03 23:41:12 +0100

    Dummy folders checked-in

--- src/main/java/sbn/gf/builder/TweetsIndexBuilder.java
+++ src/main/java/sbn/gf/builder/TweetsIndexBuilder.java
@@ -4,7 +4,6 @@ import org.apache.lucene.document.TextField;
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileInputStream;
-import java.io.FileWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.nio.file.DirectoryStream;
